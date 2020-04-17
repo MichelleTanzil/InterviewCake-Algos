8f04e7e237fece6fbec3b4a1e6b4257faea41f28
Michelle Tanzil <mtanzil01@gmail.com>
2020-04-17 06:49:15 -0700

    completed reverse string in place algo

---
+++ reverse_string_in_place/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace reverse_string_in_place
+{
+    class Program
+    {
+        public static void Reverse(char[] arrayOfChars)
+        {
+            int leftIndex = 0;
+            int rightIndex = arrayOfChars.Length - 1;
+
+            while (leftIndex < rightIndex)
+            {
+                // Swap characters
+                char temp = arrayOfChars[leftIndex];
+                arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+                arrayOfChars[rightIndex] = temp;
+
+                // Move towards middle
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
