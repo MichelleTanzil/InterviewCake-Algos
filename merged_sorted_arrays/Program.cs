d4b2644f5dc6fcaeeb527189b59dac5d9715a106
Michelle Tanzil <mtanzil01@gmail.com>
2020-04-18 08:46:58 -0700

    completed merged sorted arrays algo

---
+++ merged_sorted_arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace merged_sorted_arrays
+{
+    class Program
+    {
+        public static int[] MergeArrays(int[] myArray, int[] alicesArray)
+        {
+            // Combine the sorted arrays into one large sorted array
+            int[] finalArray = new int[myArray.Length + alicesArray.Length];
+            int
+                a = 0,
+                b = 0,
+                i = 0;
+
+            while (i < finalArray.Length)
+            {
+                bool isMyArrayExhausted = a >= myArray.Length;
+                bool isAlicesArrayExhausted = b >= alicesArray.Length;
+
+                if (
+                    !isMyArrayExhausted &&
+                    (isAlicesArrayExhausted || (myArray[a] < alicesArray[b]))
+                )
+                {
+                    finalArray[i] = myArray[a];
+                    a++;
+                }
+                else
+                {
+                    // Case: next comes from Alice's array
+                    finalArray[i] = alicesArray[b];
+                    b++;
+                }
+                i++;
+            }
+
+            return finalArray;
+        }
+
+        static void Main(string[] args)
+        {
+            Console
+                .WriteLine(MergeArrays(new int[] { 3, 4, 6, 10, 11, 15 },
+                new int[] { 1, 5, 8, 12, 14, 19 }));
+        }
+    }
+}
