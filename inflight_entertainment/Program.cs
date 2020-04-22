de84bb31f669f9b3d8818d7c62f9fbf1057339e6
Michelle Tanzil <mtanzil01@gmail.com>
2020-04-22 09:31:42 -0700

    completed inflight entertainment algo

---
+++ inflight_entertainment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace inflight_entertainment
+{
+    class Program
+    {
+        public static bool
+        CanTwoMoviesFillFlight(int[] movieLengths, int flightLength)
+        {
+            // Determine if two movies add up to the flight length
+            HashSet<int> moviesSeen = new HashSet<int>();
+
+            foreach (var firstMovie in movieLengths)
+            {
+                int secondMovie = flightLength - firstMovie;
+                if (moviesSeen.Contains(secondMovie))
+                {
+                    return true;
+                }
+                moviesSeen.Add(firstMovie);
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
