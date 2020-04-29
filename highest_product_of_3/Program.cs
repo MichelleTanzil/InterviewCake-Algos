41a986f6fe17d66a6da535a84523fceb0882da96
Michelle Tanzil <mtanzil01@gmail.com>
2020-04-28 18:53:58 -0700

    initialized new algo - highest product of 3

---
+++ highest_product_of_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace highest_product_of_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
