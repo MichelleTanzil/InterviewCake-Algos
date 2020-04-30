e909d8d5cac2191ef339a79c431408fbdcda8752
Michelle Tanzil <mtanzil01@gmail.com>
2020-04-30 12:14:19 -0700

    initialized new algo - product of all other numbers

---
+++ product_of_all_other_numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace product_of_all_other_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
