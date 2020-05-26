94567e40bde13d0af6d84736b08cc20db353ceee
Michelle Tanzil <mtanzil01@gmail.com>
2020-05-26 16:59:13 -0700

    compeleted delete node algo

---
+++ delete_node/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace delete_node
+{
+  class Program
+  {
+    public class LinkedListNode
+    {
+      public int Value { get; set; }
+
+      public LinkedListNode Next { get; set; }
+
+      public LinkedListNode(int value)
+      {
+        Value = value;
+      }
+    }
+
+    public static void DeleteNode(LinkedListNode nodeToDelete)
+    {
+      // Delete the input node from the linked list
+      var nextNode = nodeToDelete.Next;
+
+      if (nextNode != null)
+      {
+        nodeToDelete.Value = nextNode.Value;
+        nodeToDelete.Next = nextNode.Next;
+      }
+      else
+      {
+        throw new InvalidOperationException("Can't delete the last node with this technique!");
+      }
+
+
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
