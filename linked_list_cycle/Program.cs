bc8b355a4df8a2f7ff00991cab4e209ee6e5d8de
Michelle Tanzil <mtanzil01@gmail.com>
2020-05-27 08:45:27 -0700

    completed linked list cycle algo

---
+++ linked_list_cycle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace linked_list_cycle
+{
+  class Program
+  {
+    public class LinkedListNode
+    {
+      public int Value { get; set; }
+
+      public LinkedListNode Next { get; set; }
+
+      public LinkedListNode(int value)
+      {
+        Value = value;
+      }
+    }
+
+    public static bool ContainsCycle(LinkedListNode firstNode)
+    {
+      // Check if the linked list contains a cycle
+      if (firstNode == null)
+        return false;
+      else
+      {
+        var slowRunner = firstNode;
+        var fastRunner = firstNode;
+        while (fastRunner != null && fastRunner.Next != null)
+        {
+          slowRunner = slowRunner.Next;
+          fastRunner = fastRunner.Next.Next;
+          if (slowRunner == fastRunner)
+            return true;
+        }
+        return false;
+      }
+
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
