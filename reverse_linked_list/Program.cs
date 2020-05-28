6107ba4d1c9e4205aacbc7d54d1cfc9cd3e92b74
Michelle Tanzil <mtanzil01@gmail.com>
2020-05-28 06:18:25 -0700

    completed reverse linked list algo

---
+++ reverse_linked_list/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace reverse_linked_list
+{
+    class Program
+    {
+          public class LinkedListNode
+    {
+        public int Value { get; set; }
+
+        public LinkedListNode Next { get; set; }
+
+        public LinkedListNode(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public static LinkedListNode Reverse(LinkedListNode headOfList)
+    {
+        // Reverse the linked list in place
+        if(headOfList == null){
+            return null;
+        }
+        else if(headOfList.Next == null){
+            return headOfList;
+        }
+        else{
+            LinkedListNode currentNode = headOfList;
+            LinkedListNode previousNode = null;
+            LinkedListNode nextNode = null;
+            while(currentNode != null){
+                nextNode = currentNode.Next;
+                currentNode.Next = previousNode;
+
+                previousNode = currentNode;
+                currentNode = nextNode;
+            }
+            return previousNode;
+        }
+    }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
